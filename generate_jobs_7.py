5dba44274d1c4b644c08f462d6ff16486ba09d69
Andreas Haller <andreas.haller@uni.lu>
2024-04-16 19:03:38 +0200

    .

--- generate_jobs_7.py
+++ generate_jobs_7.py
@@ -21,24 +21,25 @@ with open('default.yml', 'r') as file:
 
 cfg = 0
 
-for chi_nmax in range(5, 11):
-    chi_max = 2**(chi_nmax-1)
-    chi_list = {i*10:2**i for i in range(0,chi_nmax)}
-    default['algorithm_params']['chi_list'] = chi_list
-    default['algorithm_params']['trunc_params']['chi_max'] = chi_max
+for chi_nmax in range(7, 10):
+    for ll in range(4,13):
+        chi_max = 2**(chi_nmax-1)
+        chi_list = {i*10:2**i for i in range(0,chi_nmax)}
+        default['algorithm_params']['chi_list'] = chi_list
+        default['algorithm_params']['trunc_params']['chi_max'] = chi_max
 
-    default['model_params']['Lx'] = 14
-    default['model_params']['Ly'] = 7
+        default['model_params']['Lx'] = ll
+        default['model_params']['Ly'] = 7
 
-    Bzmin = -0.7
-    Bzmax = -0.5
-    nBz = 64
-    dBz = (Bzmax-Bzmin)/nBz
-    Bzs = [np.round(Bzmin + i*dBz, decimals=4) for i in range(0, nBz)]
+        Bzmin = -0.7
+        Bzmax = -0.5
+        nBz = 16
+        dBz = (Bzmax-Bzmin)/nBz
+        Bzs = [np.round(Bzmin + i*dBz, decimals=4) for i in range(0, nBz)]
 
-    for Bz in Bzs:
-        default['model_params']['Bz'] = float(Bz)
-        with open(f'{dir_out}/{str(cfg).zfill(4)}.yml', 'w') as outfile:
-            yaml.dump(default, outfile, default_flow_style=False)
-        cfg += 1
-print(f'{cfg} configs created')
+        for Bz in Bzs:
+            default['model_params']['Bz'] = float(Bz)
+            with open(f'{dir_out}/{str(cfg).zfill(4)}.yml', 'w') as outfile:
+                yaml.dump(default, outfile, default_flow_style=False)
+            cfg += 1
+    print(f'{cfg} configs created')
