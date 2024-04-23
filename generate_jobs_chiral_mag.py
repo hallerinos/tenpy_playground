9dc186a8afbe3f0f3cf996524c52d7fc8e54a87a
Andreas Haller <andreas.haller@uni.lu>
2024-04-23 18:58:59 +0200

    .

--- generate_jobs_chiral_mag.py
+++ generate_jobs_chiral_mag.py
@@ -21,9 +21,9 @@ with open('chiral_magnet.yml', 'r') as file:
 
 cfg = 0
 
-Lxs = range(1,14)
+Lxs = range(6,11)
 
-for chi_nmax in [7,8,9]:
+for chi_nmax in [6,7,8]:
     for ll in Lxs:
         chi_max = 2**(chi_nmax-1)
         chi_list = {i*10:2**i for i in range(0,chi_nmax)}
@@ -35,7 +35,7 @@ for chi_nmax in [7,8,9]:
 
         Bzmin = -0.6
         Bzmax = -0.4
-        nBz = 8
+        nBz = 32
         dBz = (Bzmax-Bzmin)/nBz
         Bzs = [np.round(Bzmin + i*dBz, decimals=4) for i in range(0, nBz)]
 
